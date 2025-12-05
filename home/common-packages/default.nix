dfa0efe5ad929c842d744717b53420265b6906ca
partridge <anhduc130703@gmail.com>
2025-12-05 07:41:23 +0700

    fix monitors

--- home/common-packages/default.nix
+++ home/common-packages/default.nix
@@ -24,6 +24,7 @@
     vscode
 
     # misc
+    brightnessctl
     fastfetch
     hyprpaper
     ripgrep
--- users/partridge/home.nix
+++ users/partridge/home.nix
@@ -6,6 +6,7 @@
   home.sessionVariables = {
     LIBVA_DRIVER_NAME = "nvidia";
     __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    AQ_DRM_DEVICES = "/dev/dri/card0:/dev/dri/card1";
   };
 
   programs.git = {
